07c6164fb77780bbf79a563a974a599551db9df9
Ergy Zenzo <ergyzenzo.dev@gmail.com>
2022-12-16 13:10:53 +0100

    added pictures

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,36 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const { name, portrait, city, country, tagline, price } = data;
+    
+    const picture = `assets/photographers/portraits/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+        img.setAttribute('alt', name)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        
+
+        const location = document.createElement('p');
+        location.textContent = [city +', ' + country]
+        location.classList.add('location');
+
+        const tag = document.createElement('p');
+        tag.textContent = tagline;
+        tag.classList.add('tag');
+        
+        const pricing = document.createElement('p');
+        pricing.textContent = price + "€";
+        pricing.classList.add('price');
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(location);
+        article.appendChild(tag);
+        article.appendChild(pricing);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, city, country, tagline, price, getUserCardDOM }
 }
